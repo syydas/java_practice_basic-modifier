9630ee29c1340f97aaf0d4516721756ca5915c00
Asuu <suqinzheng_0624@163.com>
2020-02-19 16:54:54 +0800

    [Aus] feat: creat class student and teacher with unchangeable name

---
+++ modifier/src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private final String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
---
+++ modifier/src/main/java/Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher {
+    private final String name;
+    private Student student;
+
+    public Teacher(String name, Student student) {
+        this.name = name;
+        this.student = student;
+    }
+}
