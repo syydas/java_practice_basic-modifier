11846972ad24484a91dc531047227400acc42a4a
Asuu <suqinzheng_0624@163.com>
2020-02-19 16:58:49 +0800

    [Aus] feat: creat two students and two teachers

--- modifier/src/main/java/Application.java
+++ modifier/src/main/java/Application.java
@@ -1,7 +1,10 @@
 public class Application {
 
   public static void main(String[] args) {
-
+    Student cindy = new Student("Cindy", 80);
+    Student mary = new Student("Mary", 95);
+    Teacher solider = new Teacher("Solid", mary);
+    Teacher bob = new Teacher("Bob", cindy);
   }
 
 }
